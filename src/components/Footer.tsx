7e65788e6a9ce466119b5b45090e788f194c333c
adrienbelcastro <adrienbelcastro1@gmail.com>
2023-12-05 00:11:14 -0500

    created footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import facebook from "";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="flex justify-center">
+        <div>
+          <h3>Address</h3>
+          <p>503 Broadway Penthouse</p>
+          <p>New York</p>
+        </div>
+        <div>
+          <h3>Hours</h3>
+          <p>503 Broadway Penthouse</p>
+          <p>New York</p>
+        </div>
+        <div>
+          <h3>Social</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
