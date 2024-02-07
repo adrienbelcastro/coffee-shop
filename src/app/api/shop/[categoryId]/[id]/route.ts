fc6068f9844938fb0750fb5ca7414187ea207a3f
adrienbelcastro <adrienbelcastro1@gmail.com>
2024-02-06 23:28:08 -0500

    altered route file to get data from new db schema

--- src/app/api/shop/[categoryId]/[id]/route.ts
+++ src/app/api/shop/[categoryId]/[id]/route.ts
@@ -1,6 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { initSupabase } from "../../../../../lib/supabase/supabaseClient";
 
+interface Product {
+  productId: number;
+  name: string;
+  description: string;
+}
+
+interface CustomizationOption {
+  id: number;
+  name: string;
+  category: string;
+}
+
 export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     const productId = res.params.id;
@@ -10,19 +22,62 @@ export const GET = async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const supabase = initSupabase();
-    const { data, error } = await supabase
-      .from("products")
+    const { data: productData, error: productError } = await supabase
+      .from<Product>("products")
       .select("*")
-      .eq("id", productId);
+      .eq("product_id", parseInt(productId))
+      .single();
 
-    if (error) {
-      console.error("Error fetching products:", error);
+    const { data: prices, error: pricesError } = await supabase
+      .from("prices")
+      .select("*")
+      .eq("product_id", productId);
+
+    const { data: sizes, error: sizesError } = await supabase
+      .from("sizes")
+      .select("*")
+      .eq("product_id", productId);
+
+    const { data: customizationOptions, error: optionsError } = await supabase
+      .from("product_customization_options")
+      .select("*")
+      .eq("product_id", productId);
+
+    if (productError || pricesError || sizesError || optionsError) {
+      console.error(
+        "Error fetching products:",
+        productError || pricesError || sizesError || optionsError
+      );
       return new Response("Error fetching products", { status: 500 });
     }
 
-    return new Response(JSON.stringify(data), { status: 200 });
+    const groupedCustomizationOptions = customizationOptions.reduce(
+      (
+        acc: { [category: string]: CustomizationOption[] },
+        option: CustomizationOption
+      ) => {
+        if (!acc[option.category]) {
+          acc[option.category] = [];
+        }
+        acc[option.category].push(option);
+        return acc;
+      },
+      {}
+    );
+
+    console.log(groupedCustomizationOptions);
+
+    return new Response(
+      JSON.stringify({
+        product: productData,
+        prices,
+        sizes,
+        customizationOptions: groupedCustomizationOptions,
+      }),
+      { status: 200 }
+    );
   } catch (error) {
-    console.error("Unexpected error in the API route:", error);
-    return new Response("Unexpected error in the API route", { status: 500 });
+    console.error(`Error fetching product details`, error);
+    return new Response("Internal Server Error", { status: 500 });
   }
 };
