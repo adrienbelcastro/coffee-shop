a3e54df7ebba4b8d47b608aca87a7d1b46b394f3
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-02-14 23:19:18 -0500

    linked card component to individual product page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,6 +3,7 @@ import beans from "../../public/assets/coffeebeans-hero.png";
 import Image from "next/image";
 import { fetchData } from "../lib/dbRequests/getData";
 import { useEffect, useState } from "react";
+import Link from "../../node_modules/next/link";
 
 const Card = () => {
   const [data, setData] = useState([]);
@@ -26,16 +27,18 @@ const Card = () => {
   return (
     <>
       {itemShowcase.map((product) => (
-        <div className="max-h-[550px] max-w-[350px] cursor-pointer transform transition duration-500  hover: hover:scale-110 shadow-lg  rounded-xl shadow-[#00000040] bg-white">
-          <Image
-            className="min-h-[240px] max-h-[350px] max-w-[250px] min-w-[230px] px-20 py-10"
-            src={beans}
-            alt="Coffee Beans"
-          />
-          <div className="flex justify-around py-8 bg-rose-50  ">
-            <h2>{product.name}</h2>
+        <Link href={`/shop/${product.category_id}/${product.product_id}`}>
+          <div className="max-h-[550px] max-w-[350px] cursor-pointer transform transition duration-500  hover: hover:scale-110 shadow-lg  rounded-xl shadow-[#00000040] bg-white">
+            <Image
+              className="min-h-[240px] max-h-[350px] max-w-[250px] min-w-[230px] px-20 py-10"
+              src={beans}
+              alt="Coffee Beans"
+            />
+            <div className="flex justify-around py-8 bg-rose-50  ">
+              <h2>{product.name}</h2>
+            </div>
           </div>
-        </div>
+        </Link>
       ))}
     </>
   );
