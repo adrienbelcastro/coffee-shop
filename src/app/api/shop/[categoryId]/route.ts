7cd5bce7e8dbbdc1f5acc4c70b23e639f5db6889
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-03-13 16:21:33 -0400

    trying to fix build error in api routes

--- src/app/api/shop/[categoryId]/route.ts
+++ src/app/api/shop/[categoryId]/route.ts
@@ -1,29 +1,33 @@
 import { initSupabase } from "../../../../lib/supabase/supabaseClient";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function (req: NextApiRequest, res: NextApiResponse) {
-  try {
-    console.log(req);
-    const categoryId = res.params.categoryId;
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    try {
+      console.log(req);
+      const categoryId = req.query.categoryId;
 
-    if (!categoryId) {
-      return res.status(400).json({ error: "Category parameter is missing" });
+      if (!categoryId) {
+        return res.status(400).json({ error: "Category parameter is missing" });
+      }
+
+      const supabase = initSupabase();
+      const { data, error } = await supabase
+        .from("products")
+        .select("*")
+        .eq("category_id", categoryId);
+
+      if (error) {
+        console.error("Error fetching products:", error);
+        return res.status(500).json({ error: "Error fetching products" });
+      }
+
+      return res.status(200).json(data);
+    } catch (error) {
+      console.error("Unexpected error in the API route:", error);
+      return res
+        .status(500)
+        .json({ error: "Unexpected error in the API route" });
     }
-
-    const supabase = initSupabase();
-    const { data, error } = await supabase
-      .from("products")
-      .select("*")
-      .eq("category_id", categoryId);
-
-    if (error) {
-      console.error("Error fetching products:", error);
-      return res.status(500).json({ error: "Error fetching products" });
-    }
-
-    return res.status(200).json(data);
-  } catch (error) {
-    console.error("Unexpected error in the API route:", error);
-    return res.status(500).json({ error: "Unexpected error in the API route" });
   }
 }
