f9a55ceb9f2662c3bad2970221948a826841a8f7
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-07-28 23:13:19 -0400

    removed handler that was causing vercel error

--- src/app/api/shop/[categoryId]/route.ts
+++ src/app/api/shop/[categoryId]/route.ts
@@ -1,33 +1,29 @@
 import { initSupabase } from "../../../../lib/supabase/supabaseClient";
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "GET") {
-    try {
-      console.log(req);
-      const categoryId = req.query.categoryId;
+export async function GET(req: Request) {
+  console.log();
+  try {
+    const categoryId = NextResponse.categoryId;
 
-      if (!categoryId) {
-        return res.status(400).json({ error: "Category parameter is missing" });
-      }
-
-      const supabase = initSupabase();
-      const { data, error } = await supabase
-        .from("products")
-        .select("*")
-        .eq("category_id", categoryId);
-
-      if (error) {
-        console.error("Error fetching products:", error);
-        return res.status(500).json({ error: "Error fetching products" });
-      }
-
-      return res.status(200).json(data);
-    } catch (error) {
-      console.error("Unexpected error in the API route:", error);
-      return res
-        .status(500)
-        .json({ error: "Unexpected error in the API route" });
+    if (!categoryId) {
+      return NextResponse.json({ error: "Category parameter is missing" });
     }
+
+    const supabase = initSupabase();
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .eq("category_id", categoryId);
+
+    if (error) {
+      console.error("Error fetching products:", error);
+      return NextResponse.json({ error: "Error fetching products" });
+    }
+
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Unexpected error in the API route:", error);
+    return NextResponse.json({ error: "Unexpected error in the API route" });
   }
 }
