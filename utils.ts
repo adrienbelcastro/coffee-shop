3595d079d54f906d0b5dcbcaac32b87e30207724
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-10 18:43:11 -0400

    change api url to match the env variable on vercel

--- utils.ts
+++ utils.ts
@@ -1,5 +1,7 @@
 const API_URL =
-  process.env.NEXT_PUBLIC_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
   "https://coffee-shop-q80suu7is-adrienbelcastros-projects.vercel.app";
+
+console.log(API_URL);
 
 export default API_URL;
