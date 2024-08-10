2878524d527cf433d983c80c2c8d13d73918c916
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-10 12:30:09 -0400

    edited utils file to always pass the url to api

--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
-const API_URL = process.env.NEXTAUTH_URL || "";
+const API_URL =
+  process.env.NEXTAUTH_URL ||
+  "https://coffee-shop-q80suu7is-adrienbelcastros-projects.vercel.app";
 
 export default API_URL;
