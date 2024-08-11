823b94ce75118887a4befbbb672f0cedcc710afe
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-11 09:36:37 -0400

    added crossOrigin and rewrites to fix cors error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://coffee-shop-h5gjmlx89-adrienbelcastros-projects.vercel.app/:path*",
+      },
+    ];
+  },
+  crossOrigin: "anonymous",
   images: {
     domains: ["images.unsplash.com"],
   },
