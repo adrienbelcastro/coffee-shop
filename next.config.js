698ae37ffd4fbcac6fb627d27b97017ff5ea1ff9
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-11 02:20:44 -0400

    added url path to prevent cors error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          "https://coffee-shop-h5gjmlx89-adrienbelcastros-projects.vercel.app/:path*",
+      },
+    ];
+  },
   images: {
     domains: ["images.unsplash.com"],
   },
