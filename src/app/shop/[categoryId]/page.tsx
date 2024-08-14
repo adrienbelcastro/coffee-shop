f178b6849158d71a86a6b6b800446ccb61b85ccb
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-14 00:13:29 -0400

    fixed link import statement

--- src/app/shop/[categoryId]/page.tsx
+++ src/app/shop/[categoryId]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Link from "../../../../node_modules/next/link";
+import Link from "next/link";
 import API_URL from "./../../../../utils";
 
 type Product = {
@@ -28,6 +28,7 @@ export default async function Page({
     return null;
   }
   const productListData = await getItems(categoryId);
+  console.log(productListData);
   return (
     <>
       <Header />
