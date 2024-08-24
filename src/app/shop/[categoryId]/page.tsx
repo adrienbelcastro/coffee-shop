62e61e45143261d6090eef2375c6af74c8e92970
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-24 17:04:46 -0400

    cosnole logged the url as well

--- src/app/shop/[categoryId]/page.tsx
+++ src/app/shop/[categoryId]/page.tsx
@@ -12,6 +12,7 @@ type Product = {
 async function getItems(categoryId: string) {
   console.log(API_URL);
   console.log(categoryId);
+  console.log(`${API_URL}/api/shop/${categoryId}`);
   const res = await fetch(`${API_URL}/api/shop/${categoryId}`);
   try {
     if (!res.ok) {
