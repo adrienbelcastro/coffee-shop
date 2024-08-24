a2813bcd3c5b4d8100c83c02e90536fe426e5a2c
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-24 00:54:05 -0400

    uncommented actual code

--- src/app/shop/[categoryId]/page.tsx
+++ src/app/shop/[categoryId]/page.tsx
@@ -1,72 +1,63 @@
-// import React from "react";
-// import Footer from "@/components/Footer";
-// import Header from "@/components/Header";
-// import Link from "next/link";
-// import API_URL from "./../../../../utils";
+import React from "react";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Link from "next/link";
+import API_URL from "./../../../../utils";
 
-// type Product = {
-//   product_id: string;
-//   name: string;
-// };
-// console.log(API_URL);
+type Product = {
+  product_id: string;
+  name: string;
+};
+console.log(API_URL);
 
-// async function getItems(categoryId: string) {
-//   const res = await fetch(`${API_URL}/api/shop/${categoryId}`);
+async function getItems(categoryId: string) {
+  const res = await fetch(`${API_URL}/api/shop/${categoryId}`);
 
-//   if (!res.ok) {
-//     const errorText = await res.text();
-//     throw new Error(`Failed to fetch data: ${errorText}`);
-//   }
+  if (!res.ok) {
+    const errorText = await res.text();
+    throw new Error(`Failed to fetch data: ${errorText}`);
+  }
 
-//   return await res.json();
-// }
+  return await res.json();
+}
 
-// export default async function Page({
-//   params: { categoryId },
-// }: {
-//   params: { categoryId: string };
-// }) {
-//   if (!API_URL) {
-//     return <p>API Is Not Defined</p>;
-//   }
-//   console.log(API_URL);
-
-//   const productListData = await getItems(categoryId);
-//   console.log(productListData);
-//   return (
-//     <>
-//       <Header />
-//       <div className="flex justify-center  bg-lightGrey h-[518px]">
-//         <div className="m-8 ">
-//           <h3 className="text-center text-3xl font-light mx-0 my-4 py-4  border-b-2 border-black">
-//             Menu
-//           </h3>
-
-//           <div className="grid grid-cols-2 gap-y-8 gap-x-64">
-//             {productListData.map((product: Product) => (
-//               <Link
-//                 key={product.product_id}
-//                 href={`/shop/${categoryId}/${product.product_id}`}
-//               >
-//                 <div className="flex gap-4 items-center">
-//                   <div className="bg-white w-24 h-24 p-4 border-2 border-white rounded-[50%]"></div>
-//                   <h3>{product.name}</h3>
-//                 </div>
-//               </Link>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//       <Footer />
-//     </>
-//   );
-// }
-
-// /app/shop/[categoryId]/page.tsx
-export default function CategoryPage({
-  params,
+export default async function Page({
+  params: { categoryId },
 }: {
   params: { categoryId: string };
 }) {
-  return <div>Page for Category ID: {params.categoryId}</div>;
+  if (!API_URL) {
+    return <p>API Is Not Defined</p>;
+  }
+  console.log(API_URL);
+
+  const productListData = await getItems(categoryId);
+  console.log(productListData);
+  return (
+    <>
+      <Header />
+      <div className="flex justify-center  bg-lightGrey h-[518px]">
+        <div className="m-8 ">
+          <h3 className="text-center text-3xl font-light mx-0 my-4 py-4  border-b-2 border-black">
+            Menu
+          </h3>
+
+          <div className="grid grid-cols-2 gap-y-8 gap-x-64">
+            {productListData.map((product: Product) => (
+              <Link
+                key={product.product_id}
+                href={`/shop/${categoryId}/${product.product_id}`}
+              >
+                <div className="flex gap-4 items-center">
+                  <div className="bg-white w-24 h-24 p-4 border-2 border-white rounded-[50%]"></div>
+                  <h3>{product.name}</h3>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
