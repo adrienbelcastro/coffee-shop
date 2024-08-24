416dd26b1d1b46606a8f47ee83dd70b74f58571b
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-23 22:45:36 -0400

    added comma after request param to fix error

--- src/app/api/shop/[categoryId]/route.ts
+++ src/app/api/shop/[categoryId]/route.ts
@@ -46,6 +46,9 @@ import { NextResponse } from "next/server";
 // }
 
 // /app/api/shop/[categoryId]/route.ts
-export async function GET(request: Request{ params }: { params: { categoryId: string } }) {
+export async function GET(
+  request: Request,
+  { params }: { params: { categoryId: string } }
+) {
   return new NextResponse(`Category ID: ${params.categoryId}`);
 }
