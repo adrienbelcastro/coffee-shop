784a6bf4e5e0339031867fedfee91c648a18b3a6
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-28 00:55:08 -0400

    changed route logic back to older version

--- src/app/api/shop/[categoryId]/route.ts
+++ src/app/api/shop/[categoryId]/route.ts
@@ -1,25 +1,40 @@
+import { NextResponse, NextRequest } from "next/server";
 import { initSupabase } from "../../../../lib/supabase/supabaseClient";
-import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
-  const categoryId = req.query;
-  console.log("Received categoryId:", categoryId);
-  if (!categoryId || typeof categoryId !== "string") {
-    return res
-      .status(400)
-      .json({ error: "Category parameter is missing or invalid" });
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { categoryId: string } }
+) {
+  try {
+    const categoryId = params.categoryId;
+
+    if (!categoryId) {
+      return NextResponse.json(
+        { error: "Category parameter is missing" },
+        { status: 400 }
+      );
+    }
+
+    const supabase = initSupabase();
+    const { data, error } = await supabase
+      .from("products")
+      .select("*")
+      .eq("category_id", categoryId);
+
+    if (error) {
+      console.error("Error fetching products:", error.message);
+      return NextResponse.json(
+        { error: "Error fetching products" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error("Unexpected error in the API route:", error);
+    return NextResponse.json(
+      { error: "Unexpected error in the API route" },
+      { status: 500 }
+    );
   }
-
-  const supabase = initSupabase();
-  const { data, error } = await supabase
-    .from("products")
-    .select("*")
-    .eq("category_id", categoryId);
-
-  if (error) {
-    console.error("Error fetching products:", error.message);
-    return res.status(500).json({ error: "Error fetching products" });
-  }
-
-  res.status(200).json(data);
 }
