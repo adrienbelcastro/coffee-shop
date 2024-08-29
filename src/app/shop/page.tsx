9d10f964a5e5ab89883c3732fdb7845d511dc04a
Adrien Belcastro <65431886+adrienbelcastro@users.noreply.github.com>
2024-08-29 16:16:40 -0400

    changed import statement for Link component

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import Link from "next/link";
+import Link from "../../../node_modules/next/link";
 import API_URL from "./../../../utils";
 
 interface Category {
